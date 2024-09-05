a3330219e6b7e6b1feda3644207d11a3bc884437
Eric Delmer Millen <ericdelmermillen@MacBook-Pro-3.local>
2024-09-05 18:35:09 -0400

    finished basic experiments with icon wallpaper effect for Blog page

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,20 +1,52 @@
 import { useEffect } from "react";
 import { scrollToTop } from "../../../utils/utils";
 import "./Blog.scss";
+import { FaGlobe, FaCode, FaCartShopping } from "react-icons/fa6";
 
 const Blog = () => {
 
+  const mapNum = 100;
+
+  // get documentHeight with useEffect to get after initial render
+  // get mapNum by dividing documentHeight by X (?)
+  // stagger horizonatal margin of icons by Y rem relative to the module of the idx
+
+  
   // scroll to top on mount
   useEffect(() => {
     scrollToTop();
   }, []);
 
+
   return (
     <>
       <div className="blog">
-        <h1 className="blog--heading">
-          From Blog
-        </h1>
+        <div className="blog__inner">
+
+          <div className="blog__wallpaper">
+            
+            {Array.from({length: mapNum}).map((_, idx) => (
+
+              <div key={idx} className="wallpaper__item">
+
+                {idx % 2 === 0
+                  ? <FaCode className="wallpaper__icon"/>
+                  : idx % 3 === 0
+                  ? <FaGlobe className="wallpaper__icon"/>
+                  : idx % 5 === 0
+                  ? <FaCartShopping className="wallpaper__icon"/>
+                  : <FaCode className="wallpaper__icon"/>
+
+                }
+              </div>
+            ))}
+            
+          </div>
+
+          <h1 className="blog__heading">
+            From Blog
+          </h1>
+        </div>
       </div>
     </>
   )};
