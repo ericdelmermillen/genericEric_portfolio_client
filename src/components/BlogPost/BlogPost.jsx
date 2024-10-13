d81de4b466c0f5fe283adf7fc321256398eb7f19
Eric Delmer Millen <ericdelmermillen@MacBook-Pro-3.local>
2024-10-13 12:06:53 -0400

    finished conditional logic to truncate title and description of blogPost

--- src/components/BlogPost/BlogPost.jsx
+++ src/components/BlogPost/BlogPost.jsx
@@ -5,37 +5,55 @@ const BlogPost = ({ description, title, videoID }) => {
   const embedUrl = `https://www.youtube.com/embed/${videoID}`;
   
   const [ showFullInfo, setShowFullInfo ] = useState(false);
-  const [ titleLineCount, setTitleLineCount ] = useState(1); 
   const [ hasLongTitle, setHasLongTitle ] = useState(false);
-  const titleRef = useRef(null); 
-  const descriptionRef = useRef(null); 
+  const [ hasLongDesc, setHasLongDesc ] = useState(false);
+  const [ windowWidth, setWindowWidth ] = useState(window.innerWidth);
 
-  const hasLongDesc = description.split("\n").length > 1;
+  const titleRef = useRef(null); 
+  const descRef = useRef(null); 
 
   const desc = showFullInfo 
     ? description 
     : description.split("\n")[0];
 
   const handleToggleShowFullInfo = () => {
-    setShowFullInfo((prev) => !prev);
+    setShowFullInfo(prev => !prev)
   };
 
-  const calculateTitleLines = () => {
+  const checkHasLongTitle = () => {
     if(titleRef.current) {
       const lineHeight = parseFloat(getComputedStyle(titleRef.current).lineHeight);
       const height = titleRef.current.getBoundingClientRect().height;
-      const titleLines = Math.round(height / lineHeight) 
-      setTitleLineCount(titleLines);
-      setHasLongTitle(titleLines > 1)
+      setHasLongTitle(height > lineHeight); 
     }
   };
 
-  useEffect(() => {
-    calculateTitleLines(); 
+  const checkHasLongDesc = () => {
+    if(descRef.current) {
+      const lineHeight = parseFloat(getComputedStyle(descRef.current).lineHeight);
+      const height = descRef.current.getBoundingClientRect().height;
+      setHasLongDesc(height > lineHeight); 
+    }
+  };
 
-    window.addEventListener("resize", calculateTitleLines);
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+    checkHasLongTitle();
+    checkHasLongDesc();
+  };
+
+  // set initial value of hasLongDescription after first render
+  useEffect(() => {
+    setHasLongDesc(description.split("\n").length > 1);
+  }, [])
+
+  //add event listener for resize of window and call handleResize for initial calculation
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    
     return () => {
-      window.removeEventListener("resize", calculateTitleLines);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
@@ -53,68 +71,59 @@ const BlogPost = ({ description, title, videoID }) => {
 
           <div className="blogPost__video-text">
             <h3 
-              className={`blogPost__video-text--title ${hasLongTitle && !showFullInfo
+              className={`blogPost__video-text--title ${hasLongTitle && !showFullInfo 
                 ? "ellipse" 
-                : ""
-              }`} ref={titleRef}
+                : ""}`
+              } 
+              ref={titleRef}
             >
               <span className="blogPost__video-text--label">Title:</span> {title}
             </h3>
 
-      
-            {desc.length && !hasLongDesc
-              ?
-                (
-                  <p className="blogPost__description ellipsis" ref={descriptionRef}>
-                    <span className="blogPost__description--label">Description:</span> {desc}
-                  </p>
-                ) 
-              : null
-            }
-
-            {desc.length && hasLongDesc && !showFullInfo
-              ?
-                (
-                  <p className="blogPost__description ellipsis" ref={descriptionRef}>
-                    <span className="blogPost__description--label">Description:</span> {desc}
-                  </p>
-                ) 
-              : null
-            }
-
-            {desc.length && hasLongDesc && showFullInfo 
-
+            {desc.length && description.split("\n").length === 1
               ? 
-                (description.split("\n").map((paragraph, idx) => 
-                    <p className="blogPost__description" key={idx} ref={descriptionRef}>
-                      {idx === 0 
-                        ? <span className="blogPost__description--label">Description:</span>
-                        : null
-                      }
-                      {paragraph}
-                  </p>
-                    
-                  )
-                )
-
-              : null
-            }
-
-            {hasLongDesc || hasLongTitle
-              ? 
-                (
-                  <button
-                    className="blogPost__show-full-info"
-                    onClick={handleToggleShowFullInfo}
+                <>
+                  <p className={`blogPost__description ${hasLongDesc && !showFullInfo 
+                    ? "ellipsis" 
+                    : ""}`}
+                    ref={descRef}
                   >
-                    {showFullInfo ? "Show Less" : "Show Full Info"}
-                  </button>
-                )
-                : null
-              }
+                    <span className="blogPost__description--label">Description:</span> {desc}
+                  </p>
+                </>
+             
+              : desc.length && description.split("\n").length > 1 && showFullInfo
+              ?
+                <>
+                  {description.split("\n").map((paragraph, idx) => 
+                    <p className="blogPost__description" key={idx}>
+                      {idx === 0
+                        ? <span className="blogPost__description--label">Description:</span> 
+                        : null
+                      } {paragraph}
+                    </p>
+                    )
+                  }
+                </>
+              : desc.length && description.split("\n").length > 1 && !showFullInfo
+              ?
+                <>
+                  <p className={`blogPost__description ${hasLongDesc && !showFullInfo 
+                    ? "ellipsis" 
+                    : ""}`}
+                    ref={descRef}
+                  >
+                    <span className="blogPost__description--label">Description:</span> {desc}
+                  </p>
+                </>
+              : null
+            }
 
-            {/* Display the number of lines for the title */}
-            {/* <p>Title spans {titleLineCount} {titleLineCount === 1 ? "line" : "lines"}</p> */}
+            {(hasLongDesc || hasLongTitle) && (
+              <button className="blogPost__show-full-info" onClick={handleToggleShowFullInfo}>
+                {showFullInfo ? "Show Less" : "Show Full Info"}
+              </button>
+            )}
           </div>
         </div>
       </div>
