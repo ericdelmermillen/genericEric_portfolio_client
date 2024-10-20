0be48f97818f6d29bea5a044e87eff02929cd84b
Eric Delmer Millen <ericdelmermillen@MacBook-Pro-3.local>
2024-10-20 17:51:46 -0400

    transitions now seem to be working for LightBox when moving in forward position: requires further testing

--- src/components/LightBox/LightBox.jsx
+++ src/components/LightBox/LightBox.jsx
@@ -7,43 +7,67 @@ const LightBoxImage = ({
   idx, 
   currentIdx,
   maxIdx,
-  isMovingForward
+  isMovingForward,
+  isInitialView
  }) => {
 
   const isCurrentImage = idx === currentIdx;
+  const isLastImage = currentIdx === maxIdx;
 
   useEffect(() => {
     const lightBoxImage = document.getElementById(`${img.id}`);
 
-    if(isMovingForward) {
+    if(isMovingForward && isInitialView) {
+      if(isCurrentImage) {
+        lightBoxImage.classList.add("current");
+      } else if(!isCurrentImage) {
+        lightBoxImage.classList.add("left");
+      }      
+    }
+
+    if(isMovingForward && !isInitialView) {
 
       if(currentIdx === 0 && (idx + 1) === maxIdx) {
-        // console.log(img.id)
-        // console.log(idx)
-        // console.log(maxIdx)
         lightBoxImage.classList.remove("current");
         lightBoxImage.classList.add("right");
-      }
-    
+        setTimeout(() => {
+          lightBoxImage.classList.remove("right");
+          lightBoxImage.classList.add("left");
+        }, 1000)
+      } else 
+
       if(isCurrentImage) {
+        // console.log(img.id)
+        lightBoxImage.classList.remove("left");
         lightBoxImage.classList.add("current");
       } else if(idx < currentIdx) {
         lightBoxImage.classList.remove("current");
         lightBoxImage.classList.add("right");
         setTimeout(() => {
           lightBoxImage.classList.remove("right");
+          lightBoxImage.classList.add("left");
         }, 1000)
       } else if((idx + 1) === maxIdx && currentIdx === 0) {
         console.log(img)
         lightBoxImage.classList.remove("current");
         lightBoxImage.classList.add("right");
+        lightBoxImage.classList.remove("left");
         setTimeout(() => {
           lightBoxImage.classList.remove("right");
         }, 1000)
       }
-       else {
-        // console.log(img)
-        lightBoxImage.classList.remove("right");
+       else if(idx === maxIdx && currentIdx === 0) {
+        console.log(idx)
+        console.log(maxIdx)
+        console.log("here")
+        lightBoxImage.classList.add("right");
+        setTimeout(() => {
+          lightBoxImage.classList.remove("right");
+          lightBoxImage.classList.add("left");
+
+        }, 1000)
+        // lightBoxImage.classList.remove("right");
+        // lightBoxImage.classList.add("left");
       }
 
     } 
@@ -86,8 +110,9 @@ const LightBox = ({
   // const { scrollYPos, prevScrollYPos } = useAppContext();
   const [ fadeOpacity, setFadeOpacity ] = useState(false);
   const [ isMovingForward, setIsMovingForward ] = useState(true);
+  const [ isInitialView, setIsInitialView ] = useState(true);
 
-  const maxIdx = images.length;
+  const maxIdx = images.length - 1;
  
   const handleOverlayClick = () => {
     handleSetShowLightBoxFalse();
@@ -95,17 +120,26 @@ const LightBox = ({
   };
 
   const handlePrevClick = () => {
-    console.log("prev");
+    if(isInitialView) {
+      setIsInitialView(false)
+    }
+    
     if(isMovingForward) {
       setIsMovingForward(false)
     };
+
     handleDecrementCurrentIdx();
   };
 
   const handleNextClick = () => {
+    if(isInitialView) {
+      setIsInitialView(false)
+    }
+    
     if(!isMovingForward) {
       setIsMovingForward(true)
     };
+
     handleIncrementCurrentIdx();
   };
 
@@ -149,6 +183,7 @@ const LightBox = ({
                 currentIdx={currentIdx}
                 isMovingForward={isMovingForward}
                 maxIdx={maxIdx}
+                isInitialView={isInitialView}
                 />
               <LightBoxImage 
                 img={images[1]}
@@ -156,6 +191,7 @@ const LightBox = ({
                 currentIdx={currentIdx}
                 isMovingForward={isMovingForward}
                 maxIdx={maxIdx}
+                isInitialView={isInitialView}
                 />
               <LightBoxImage 
                 img={images[2]}
@@ -163,6 +199,7 @@ const LightBox = ({
                 currentIdx={currentIdx}
                 isMovingForward={isMovingForward}
                 maxIdx={maxIdx}
+                isInitialView={isInitialView}
                 />
               <LightBoxImage 
                 img={images[3]}
@@ -170,6 +207,7 @@ const LightBox = ({
                 currentIdx={currentIdx}
                 isMovingForward={isMovingForward}
                 maxIdx={maxIdx}
+                isInitialView={isInitialView}
                 />
               <LightBoxImage 
                 img={images[4]}
@@ -177,6 +215,7 @@ const LightBox = ({
                 currentIdx={currentIdx}
                 isMovingForward={isMovingForward}
                 maxIdx={maxIdx}
+                isInitialView={isInitialView}
                 />
               <LightBoxImage 
                 img={images[5]}
@@ -184,6 +223,7 @@ const LightBox = ({
                 currentIdx={currentIdx}
                 isMovingForward={isMovingForward}
                 maxIdx={maxIdx}
+                isInitialView={isInitialView}
               />
 
               <button
