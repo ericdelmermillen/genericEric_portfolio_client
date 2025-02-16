daaf1fc35bf3f2340f8741b5386355932d4f4f12
Eric Delmer Millen <ericdelmermillen@Erics-MBP---3.local>
2025-02-16 12:11:56 -0500

    commit after testing s3 sdk package updates: not pushed

--- src/components/ProjectsFeed/ProjectsFeed.jsx
+++ src/components/ProjectsFeed/ProjectsFeed.jsx
@@ -123,12 +123,12 @@ const ProjectsFeed = () => {
     };
   };
 
-  // // fetch nest page useEffect
-  // useEffect(() => {
-  //   if(!isFinalPageFetched) {
-  //     fetchProjects();
-  //   };
-  // }, [page]);
+  // // fetch next page useEffect
+  useEffect(() => {
+    if(!isFinalPageFetched) {
+      fetchProjects();
+    };
+  }, [page]);
 
   return (
     <>
